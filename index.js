989c190004f4f62e476c0e6fc437b5a7deda6aa8
jesse keane <me@jessekeane.me>
2013-06-30 04:22:13 +0400

    working errors and verbosity

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var c = require('commander'),
     path = require('path');
 
 c
-  .version('0.0.2')
+  .version('0.0.3')
   .option('-d, --dir [dirname]', 'serve files from [dirname] | default cwd')
   .option('-p, --port [num]', 'serve on port [num] | default 61403', parseInt)
   .option('-v, --verbose', 'log connections to console | default off')
@@ -22,19 +22,22 @@ if (!dir.match(/^\//) || dir.match(/^\./)) {
 
 http.createServer(function (req, res) {
   if (req.method.toLowerCase() != 'get') {
+    console.log(req.socket.remoteAddress + ' tried to ' + req.method.toLowerCase() + ' ' + dir + parse(req.url).pathname);
     showError(403, res);
     return;
   }
   var reqPath = parse(req.url).pathname;
-  fs.exists(dir + reqPath, function (exists) {
-    if (!exists) {
+  fs.stat(dir + reqPath, function (err, stat) {
+    if (err || !stat.isFile()) {
+      console.log(req.socket.remoteAddress + ' 404 on ' + dir + reqPath);
       showError(404, res);
       return;
     }
+    if (verbose) { console.log(req.socket.remoteAddress + ' reading ' + dir + reqPath); }
     fs.createReadStream(dir + reqPath).pipe(res);
   });
 }).listen(port);
 
 function showError(errorCode, res) {
-  fs.createReadStream('../errors/' + errorCode + '.html').pipe(res);
+  fs.createReadStream(__dirname + '/errors/' + errorCode + '.html').pipe(res);
 }

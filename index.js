f8e69f133efe3b23b848485317b2dbd4921f60b6
jesse keane <me@jessekeane.me>
2013-06-30 08:56:16 +0400

    providing api

--- index.js
+++ index.js
@@ -4,20 +4,9 @@ var c = require('commander'),
     mime = require('mime'),
     parse = require('url').parse,
     http = require('http'),
+    EventEmitter = require('events').EventEmitter,
+    util = require('util'),
     path = require('path');
-
-c
-  .version('0.0.5')
-  .option('-d, --dir [dirname]', 'serve files from [dirname] | default cwd')
-  .option('-p, --port [num]', 'serve on port [num] | default 61403', parseInt)
-  .option('-v, --verbose', 'log connections to console | default off')
-  .parse(process.argv);
-
-new Glance({
-  port: c.port,
-  dir: c.dir,
-  verbose: c.verbose
-});
 
 function Glance(options) {
 
@@ -29,39 +18,79 @@ function Glance(options) {
     this.dir = path.normalize(this.dir);
   }
 
-  http.createServer(function (req, res) { 
-    var reqPath = parse(req.url).pathname,
-        fullPath = this.dir + reqPath,
-        ip = req.socket.remoteAddress,
-        method = req.method.toLowerCase();
-    if (method != 'get') {
-      if (this.verbose) { console.log(ip + ' tried to ' + method + ' ' + fullPath); }
-      showError(403, res);
+  return this;
+}
+
+util.inherits(Glance, EventEmitter);
+
+Glance.prototype.start = function () {
+  this.on('error', function (errorCode, request) {
+    if (this.verbose) { console.log(request.ip + ' error ' + errorCode + ' on ' + request.fullPath); }
+    showError(errorCode, request.response);
+  });
+
+  this.on('read', function (request) {
+    if (this.verbose) { console.log(request.ip + ' read ' + request.fullPath); }
+  });
+
+  this.server = http.createServer(function (req, res) { 
+    var request = {
+        reqPath: parse(req.url).pathname,
+        fullPath: this.dir + reqPath,
+        ip: req.socket.remoteAddress,
+        method: req.method.toLowerCase(),
+        response: res
+    };
+    if (request.method != 'get') {
+      this.emit('error', 403, request);
       return;
     }
     fs.stat(fullPath, function (err, stat) {
       if (err) {
-        if (this.verbose) { console.log(ip + ' 404 on ' + fullPath); }
-        showError(404, res);
+        this.emit('error', 404, request);
         return;
       }
       if (stat.isDirectory()) {
-        if (this.verbose) { console.log(ip + ' attempt dir list on ' + fullPath); }
-        showError('no-index', res);
+        this.emit('error', 'no-index', request);
         return;
       }
-      if (this.verbose) { console.log(ip + ' reading ' + fullPath); }
+      this.emit('read', request);
+      
       res.setHeader('Content-Type', mime.lookup(fullPath));
       fs.createReadStream(fullPath).pipe(res);
+    
     }.bind(this));
+
   }.bind(this)).listen(this.port);
-}
+
+};
+
+Glance.prototype.stop = function () {
+  this.server.close();
+};
 
 function showError(errorCode, res) {
   fs.createReadStream(__dirname + '/errors/' + errorCode + '.html').pipe(res);
 }
 
+
 module.exports.createGlance = function (options) {
   return new Glance(options);
 };
+
 module.exports.Glance = Glance;
+
+c
+  .version('0.0.5')
+  .option('-d, --dir [dirname]', 'serve files from [dirname] | default cwd')
+  .option('-p, --port [num]', 'serve on port [num] | default 61403', parseInt)
+  .option('-v, --verbose', 'log connections to console | default off')
+  .parse(process.argv);
+
+var glance = new Glance({
+  port: c.port,
+  dir: c.dir,
+  verbose: c.verbose
+});
+glance.start();
+

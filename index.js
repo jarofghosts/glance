c85730ff17f834795c874fcf617b192dee8d7662
jesse keane <me@jessekeane.me>
2013-06-30 06:52:21 +0400

    send mime types

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 var c = require('commander'),
     fs = require('fs'),
+    mime = require('mime'),
     parse = require('url').parse,
     http = require('http'),
     path = require('path');
 
 c
-  .version('0.0.3')
+  .version('0.0.4')
   .option('-d, --dir [dirname]', 'serve files from [dirname] | default cwd')
   .option('-p, --port [num]', 'serve on port [num] | default 61403', parseInt)
   .option('-v, --verbose', 'log connections to console | default off')
@@ -20,20 +21,24 @@ if (!dir.match(/^\//) || dir.match(/^\./)) {
   dir = path.normalize(dir);
 }
 
-http.createServer(function (req, res) {
-  if (req.method.toLowerCase() != 'get') {
-    console.log(req.socket.remoteAddress + ' tried to ' + req.method.toLowerCase() + ' ' + dir + parse(req.url).pathname);
+http.createServer(function (req, res) { 
+  var reqPath = parse(req.url).pathname,
+      fullPath = dir + reqPath,
+      ip = req.socket.remoteAddress,
+      method = req.method.toLowerCase();
+  if (method != 'get') {
+    if (verbose) { console.log(ip + ' tried to ' + method + ' ' + fullPath); }
     showError(403, res);
     return;
   }
-  var reqPath = parse(req.url).pathname;
-  fs.stat(dir + reqPath, function (err, stat) {
+  fs.stat(fullPath, function (err, stat) {
     if (err || !stat.isFile()) {
-      console.log(req.socket.remoteAddress + ' 404 on ' + dir + reqPath);
+      if (verbose) { console.log(ip + ' 404 on ' + fullPath); }
       showError(404, res);
       return;
     }
-    if (verbose) { console.log(req.socket.remoteAddress + ' reading ' + dir + reqPath); }
+    if (verbose) { console.log(ip + ' reading ' + fullPath); }
+    res.sendHeader('Content-Type', mime.lookup(fullPath));
     fs.createReadStream(dir + reqPath).pipe(res);
   });
 }).listen(port);
@@ -41,3 +46,4 @@ http.createServer(function (req, res) {
 function showError(errorCode, res) {
   fs.createReadStream(__dirname + '/errors/' + errorCode + '.html').pipe(res);
 }
+

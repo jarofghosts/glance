c67631925cf30a2b6347e005e0a9750e209c1221
jesse keane <me@jessekeane.me>
2013-07-03 05:15:10 +0400

    0.1.10

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ module.exports.Glance = Glance;
 
   if (require.main === module) {
   c
-    .version('0.1.9')
+    .version('0.1.10')
     .option('-d, --dir [dirname]', 'serve files from [dirname] | default cwd')
     .option('-i, --indexing', 'turn on autoindexing for directory requests | default off')
     .option('-n, --nodot', 'do not list or serve dotfiles | default off')

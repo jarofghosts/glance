672e15de74abcd8d2b77e4072ee7d7544f3235b2
jesse keane <me@jessekeane.me>
2013-09-01 04:26:11 +0400

    move to separate function

--- index.js
+++ index.js
@@ -47,46 +47,10 @@ Glance.prototype.start = function () {
     if (this.verbose) { colorConsole.log(['#green[INFO] ', request.ip, ' read #bold[', request.fullPath, ']'].join('')); }
   });
 
-  this.server = http.createServer(function (req, res) { 
-    var request = {
-        fullPath: this.dir + decodeURIComponent(parse(req.url).pathname),
-        ip: req.socket.remoteAddress,
-        method: req.method.toLowerCase(),
-        response: res
-    };
-    if (request.method != 'get') {
-      this.emit('error', 405, request);
-      return;
-    }
-    if (this.nodot && path.basename(request.fullPath).match(/^\./)) {
-      this.emit('error', 404, request);
-      return;
-    }
-    fs.stat(request.fullPath, function (err, stat) {
-      if (err) {
-        this.emit('error', 404, request);
-        return;
-      }
-      if (stat.isDirectory()) {
-        if (this.indexing) {
-          var listPath = request.fullPath.replace(/\/$/, '');
-          res.writeHead(200, { "Content-Type": "text/html" });
-          htmlls(listPath, { hideDot: this.nodot }).pipe(res);
-          this.emit('read', request);
-        } else {
-          this.emit('error', 403, request);
-        }
-        return;
-      }
-      this.emit('read', request);
-      
-      res.writeHead(200, { 'Content-Type': mime.lookup(request.fullPath) });
-      fs.createReadStream(request.fullPath).pipe(res);
-    
-    }.bind(this));
+  this.server = http.createServer(this.serveRequest.bind(this)).listen(this.port);
 
-  }.bind(this)).listen(this.port);
   if (isCli || this.verbose) { colorConsole.log(['#magenta[glance] serving #bold[', this.dir, '] on port #green[', this.port, ']'].join('')); }
+
 };
 
 Glance.prototype.stop = function () {

c863cd4887cedfb21f2ebae57df69cbb68e65ab3
jesse keane <me@jessekeane.me>
2013-10-20 21:59:17 -0700

    added command

---
+++ bin/glance.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var Glance = require('../').Glance,
+    c = require('commander'),
+    path = require('path'),
+    globalConfigFile = path.join(
+      path.normalize(process.env.HOME || process.env.USERPROFILE),
+      '.glance.json'
+    ),
+    version = require('../package.json').version
+
+try {
+  var globalConfig = require(globalConfigFile)
+  defaults = xtend(defaults, globalConfig)
+} catch (e) {}
+try {
+  var localConfig = require(path.join(process.cwd(), '.glance.json'))
+  defaults = xtend(defaults, localConfig)
+} catch (e) {}
+
+c
+  .version(version)
+  .option('-d, --dir [dirname]',
+      'serve files from [dirname] | default cwd')
+  .option('-i, --indexing <files>',
+      'turn on autoindexing for directory requests | default off')
+  .option('-I, --indices',
+      'comma-separated list of files considered as an "index"')
+  .option('-n, --nodot',
+      'do not list or serve dotfiles | default off')
+  .option('-p, --port [num]',
+      'serve on port [num] | default 61403', parseInt)
+  .option('-v, --verbose',
+      'log connections to console | default off')
+  .parse(process.argv)
+
+var cliOptions = {}
+
+if (c.dir !== undefined) cliOptions.dir = c.dir
+if (c.indexing !== undefined) cliOptions.indexing = c.indexing
+if (c.indices !== undefined) cliOptions.indices = c.indices.split(',')
+if (c.nodot !== undefined) cliOptions.nodot = c.nodot
+if (c.port !== undefined) cliOptions.port = c.port
+if (c.verbose !== undefined) cliOptions.verbose = c.verbose
+
+new Glance(cliOptions).start()
+
